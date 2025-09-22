ca7839f25ac4f76ed55040f7b77d0453dfa32242
corentinlger <corentin.lger@gmail.com>
2025-09-22 16:35:52 +0000

    Add test file for ramp setup with openfe

---
+++ test_openfe_in_setup.py
@@ -0,0 +1,18 @@
+import rampds as rs
+from rampds.openfe_utils.utils import RAMP_SETUP_KITS_NAS_PATH, RAMP_KITS_NAS_PATH
+
+ramp_kit = "kaggle_abalone"
+setup_root = RAMP_SETUP_KITS_NAS_PATH
+kit_root = "./"
+version = "OpenFE_test"
+number = 0
+
+# Use the original setup function
+rs.setup(
+    ramp_kit=ramp_kit,
+    setup_root=setup_root,
+    kit_root=kit_root,
+    version=version,          
+    number=number,
+    openfe_feature_engineering=True
+)
